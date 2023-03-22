504f9cd2e602a762ac61804e3d28acde37f91e42
AkashRoy5680 <royakash1272@gmail.com>
2023-03-22 12:55:53 +0100

    nav styling done

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,19 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+
+type Props = {
+    page:string;
+}
+
+const Link = ({page}: Props) => {
+const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
+  return (
+    <AnchorLink className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""} transition duration-500 hover:text-primary-300 `}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}>
+        {page}
+    </AnchorLink>
+  )
+}
+
+export default Link;
